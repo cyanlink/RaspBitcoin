38548f175b1fd248432bd1e6223601adfbd6ec7c
linkft <tgvita@outlook.com>
2020-10-26 21:43:45 +0800

    fix

--- RBImage.py
+++ RBImage.py
@@ -1,11 +1,9 @@
 from PIL import Image,ImageOps
-from lib import epd2in13_V2
 
 SCREEN_WIDTH = 250
 SCREEN_HEIGHT = 122
 
-class RBImage(Image):
-
+class RBImage(Image.Image):
     def new(mode, size, color=0):
         image = Image.new(mode, size, color)
         return RBImage(image.transpose(Image.ROTATE_180))
@@ -15,7 +13,7 @@ class RBImage(Image):
         ratio_width = SCREEN_WIDTH/self.size[0]
         ratio_height = SCREEN_HEIGHT/self.size[1]
         ratio = ratio_width if ratio_width < ratio_height else ratio_height
-        
+
         new_size = tuple([int(x*ratio) for x in self.size])
         new_image = ImageOps.pad(self.resize(new_size, Image.ANTIALIAS), (SCREEN_WIDTH, SCREEN_HEIGHT), color='white')
         return new_image
